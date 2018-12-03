d485ad286317a81855aa3a8fae733e115941375e
Anton Mashko <anton.mashko@gmail.com>
2018-12-03 23:14:05 +0200

    expand package with level methods; added printers

--- inner.go
+++ inner.go
@@ -24,28 +24,35 @@ func (e *empty) Copy(prefix string) cpPrinter { return e }
 
 type inner struct {
 	logger *l.Logger
-	prefix string
 	level  string
+	prefix string
 }
 
 func newInner(out io.Writer, prefix string, level Level, flag int) *inner {
+	var lvl string
+	if level <= DebugLevel && level > OffLevel {
+		lvl = fmt.Stringer(level).String() + " "
+	}
+	if prefix != "" {
+		prefix += " "
+	}
 	return &inner{
-		level:  fmt.Stringer(level).String(),
+		level:  lvl,
 		prefix: prefix,
 		logger: l.New(out, "", flag),
 	}
 }
 
 func (i *inner) Print(args ...interface{}) {
-	i.logger.Output(2, i.level+" "+i.prefix+" "+fmt.Sprint(args...))
+	i.logger.Output(2, i.level+i.prefix+fmt.Sprint(args...))
 }
 
 func (i *inner) Printf(format string, args ...interface{}) {
-	i.logger.Output(2, i.level+" "+i.prefix+" "+fmt.Sprintf(format, args...))
+	i.logger.Output(2, i.level+i.prefix+fmt.Sprintf(format, args...))
 }
 
 func (i *inner) Println(args ...interface{}) {
-	i.logger.Output(2, i.level+" "+i.prefix+" "+fmt.Sprintln(args...))
+	i.logger.Output(2, i.level+i.prefix+fmt.Sprintln(args...))
 }
 
 func (i *inner) Copy(prefix string) cpPrinter {
@@ -67,29 +74,26 @@ func New(prefix string, level Level) Logger {
 func NewWithOutput(out io.Writer, prefix string, level Level, flag int) Logger {
 	e := &empty{}
 	l := &logger{fatal: e, err: e, warn: e, info: e, debug: e}
-	if level == Off {
+	if level == OffLevel {
 		return l
 	}
-	if level <= Off {
+	l.fatal = newInner(out, prefix, FatalLevel, flag)
+	if level == FatalLevel {
 		return l
 	}
-	l.fatal = newInner(out, prefix, Fatal, flag)
-	if level <= Fatal {
+	l.err = newInner(out, prefix, ErrorLevel, flag)
+	if level == ErrorLevel {
 		return l
 	}
-	l.err = newInner(out, prefix, Error, flag)
-	if level <= Error {
+	l.warn = newInner(out, prefix, WarnLevel, flag)
+	if level == WarnLevel {
 		return l
 	}
-	l.warn = newInner(out, prefix, Warn, flag)
-	if level <= Warn {
+	l.info = newInner(out, prefix, InfoLevel, flag)
+	if level <= InfoLevel {
 		return l
 	}
-	l.info = newInner(out, prefix, Info, flag)
-	if level <= Info {
-		return l
-	}
-	l.debug = newInner(out, prefix, Debug, flag)
+	l.debug = newInner(out, prefix, DebugLevel, flag)
 	return l
 }
 
@@ -102,6 +106,12 @@ func (l *logger) Copy(prefix string) Logger {
 		debug: l.debug.Copy(prefix),
 	}
 }
+
+func (l *logger) DebugPrinter() Printer { return l.debug }
+func (l *logger) InfoPrinter() Printer  { return l.info }
+func (l *logger) WarnPrinter() Printer  { return l.warn }
+func (l *logger) ErrorPrinter() Printer { return l.err }
+func (l *logger) FatalPrinter() Printer { return l.fatal }
 
 func (l *logger) Debug(args ...interface{})                 { l.debug.Print(args...) }
 func (l *logger) Debugf(format string, args ...interface{}) { l.debug.Printf(format, args...) }
--- logger.go
+++ logger.go
@@ -1,36 +1,53 @@
 package log
 
+import (
+	"errors"
+	l "log"
+	"os"
+)
+
+const (
+	OffLevel Level = iota
+	FatalLevel
+	ErrorLevel
+	WarnLevel
+	InfoLevel
+	DebugLevel
+)
+
+const (
+	OffLevelString   = "OFF"
+	FatalLevelString = "FATAL"
+	ErrorLevelString = "ERROR"
+	WarnLevelString  = "WARN"
+	InfoLevelString  = "INFO"
+	DebugLevelString = "DEBUG"
+)
+
+var ErrInvalidStringLevel = errors.New("string level doesn't match to any of levels")
+
+var DefaultLogger Logger = NewWithOutput(os.Stderr, "", InfoLevel, l.LstdFlags)
+
 type Level byte
 
 func (l Level) String() string {
 	switch l {
-	case Off:
-		return "OFF"
-	case Fatal:
-		return "FATAL"
-	case Error:
-		return "ERROR"
-	case Warn:
-		return "WARN"
-	case Info:
-		return "INFO"
-	case Debug:
-		return "DEBUG"
+	case OffLevel:
+		return OffLevelString
+	case FatalLevel:
+		return FatalLevelString
+	case ErrorLevel:
+		return ErrorLevelString
+	case WarnLevel:
+		return WarnLevelString
+	case InfoLevel:
+		return InfoLevelString
+	case DebugLevel:
+		return DebugLevelString
 	default:
 		return "UNKNOWN"
 	}
 }
-
-const (
-	Off Level = iota
-	Fatal
-	Error
-	Warn
-	Info
-	Debug
-)
-
-//var DefaultLogger Logger = &inner{}
 
 type Printer interface {
 	Print(...interface{})
@@ -58,4 +75,70 @@ type Logger interface {
 	Fatal(...interface{})
 	Fatalf(string, ...interface{})
 	Fatalln(...interface{})
+}
+
+type Printers interface {
+	DebugPrinter() Printer
+	InfoPrinter() Printer
+	WarnPrinter() Printer
+	ErrorPrinter() Printer
+	FatalPrinter() Printer
+}
+
+func DefaultPrinters() Printers {
+	prs, ok := DefaultLogger.(Printers)
+	if !ok {
+		return nil
+	}
+	return prs
+}
+
+func ParseLevel(level string) (Level, error) {
+	switch level {
+	case OffLevelString:
+		return OffLevel, nil
+	case FatalLevelString:
+		return FatalLevel, nil
+	case ErrorLevelString:
+		return ErrorLevel, nil
+	case WarnLevelString:
+		return WarnLevel, nil
+	case InfoLevelString:
+		return InfoLevel, nil
+	case DebugLevelString:
+		return DebugLevel, nil
+	default:
+		return OffLevel, ErrInvalidStringLevel
+	}
+}
+
+func Debug(args ...interface{})                 { DefaultLogger.Debug(args...) }
+func Debugf(format string, args ...interface{}) { DefaultLogger.Debugf(format, args...) }
+func Debugln(args ...interface{})               { DefaultLogger.Debugln(args...) }
+
+func Info(args ...interface{})                 { DefaultLogger.Info(args...) }
+func Infof(format string, args ...interface{}) { DefaultLogger.Infof(format, args...) }
+func Infoln(args ...interface{})               { DefaultLogger.Infoln(args...) }
+
+func Warn(args ...interface{})                 { DefaultLogger.Warn(args...) }
+func Warnf(format string, args ...interface{}) { DefaultLogger.Warnf(format, args...) }
+func Warnln(args ...interface{})               { DefaultLogger.Warnln(args...) }
+
+func Error(args ...interface{})                 { DefaultLogger.Error(args...) }
+func Errorf(format string, args ...interface{}) { DefaultLogger.Errorf(format, args...) }
+func Errorln(args ...interface{})               { DefaultLogger.Errorln(args...) }
+
+func Fatal(args ...interface{}) {
+	DefaultLogger.Fatal(args...)
+	os.Exit(1)
+}
+
+func Fatalf(format string, args ...interface{}) {
+	DefaultLogger.Fatalf(format, args...)
+	os.Exit(1)
+}
+
+func Fatalln(args ...interface{}) {
+	DefaultLogger.Fatalln(args...)
+	os.Exit(1)
 }
